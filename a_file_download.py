89a348913cd4ed63d03b5ffbc7680e19b7e4d980
yuki-0819 <good-boy_4649@outlook.jp>
2024-10-29 22:27:13 +0900

    commit

--- a_file_download.py
+++ a_file_download.py
@@ -1,8 +1,8 @@
-from yt_dlp import youtubedl
+from yt_dlp import YoutubeDL as y_dl
 
 #最高の画質と音質を動画をダウンロードする
 ydl_opts = {'format': 'best'}
 
 #動画のURLを指定
-with youtubedl(ydl_opts) as ydl:
+with y_dl(ydl_opts) as ydl:
     ydl.download(['hhttps://youtu.be/5wEtefq9VzM'])
